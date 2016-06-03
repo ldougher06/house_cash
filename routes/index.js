1bcc06fc8f66e4bb85f72e2b2fbcc2980c1cb5d5
logan dougher <logan.dougher@gmail.com>
2016-06-03 14:40:40 -0500

    added initial crud opperations w/ pg. need to create a test table

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,159 @@
 const express = require('express');
 const router  = express.Router();
 const path = require('path');
+const pg = require('pg');
+const connectionString = require(path.join(__dirname, '../', 'config'));
+
 
 router.get('/', (req, res) => {
   // res.render('index');
   res.sendFile(path.join(__dirname, '../', 'public', 'views', 'index.html'));
 
 });
+
+router.post('/api/v1/bill', function(req, res) {
+
+    var results = [];
+
+    // Grab data from http request
+    var data = {text: req.body.text, complete: false};
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+          return res.status(500).json({ success: false, data: err});
+        }
+
+        // SQL Query > Insert Data
+        client.query("INSERT INTO invoices(text, complete) values($1, $2)", [data.text, data.complete]);
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM invoices ORDER BY id ASC");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.json(results);
+        });
+
+
+    });
+});
+
+router.get('/api/v1/bill', function(req, res) {
+
+    var results = [];
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+          return res.status(500).json({ success: false, data: err});
+        }
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM invoices ORDER BY id ASC;");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.json(results);
+        });
+
+    });
+
+});
+
+router.put('/api/v1/bill/:bill_id', function(req, res) {
+
+    var results = [];
+
+    // Grab data from the URL parameters
+    var id = req.params.todo_id;
+
+    // Grab data from http request
+    var data = {text: req.body.text, complete: req.body.complete};
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+          return res.status(500).send(json({ success: false, data: err}));
+        }
+
+        // SQL Query > Update Data
+        client.query("UPDATE invoices SET text=($1), complete=($2) WHERE id=($3)", [data.text, data.complete, id]);
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM invoices ORDER BY id ASC");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.json(results);
+        });
+    });
+
+});
+
+router.delete('/api/v1/bill/:bill_id', function(req, res) {
+
+    var results = [];
+
+    // Grab data from the URL parameters
+    var id = req.params.bill_id;
+
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+          return res.status(500).json({ success: false, data: err});
+        }
+
+        // SQL Query > Delete Data
+        client.query("DELETE FROM invoices WHERE id=($1)", [id]);
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM invoices ORDER BY id ASC");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.json(results);
+        });
+    });
+
+});
+
 
 module.exports = router;
